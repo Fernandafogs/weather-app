1716e6d826eea5be53859aa235ec9feb2549f4d8
Fernanda Fogaca <fernanda.ramos.br@gmail.com>
2024-01-09 12:44:58 +0000

    adicionando mensagem de erro

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -21,35 +21,43 @@ const WeatherApp = () => {
         if(element[0].value === "") {
             return alert("Please enter a city");
         }
+
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
         let response = await fetch(url);
         let data = await response.json();
-        const humidity = document.getElementsByClassName("humidity-percent");
-        const wind = document.getElementsByClassName("wind-rate");
-        const temperature = document.getElementsByClassName("weather-temp");
-        const location = document.getElementsByClassName("weather-location");
-        
-        humidity[0].innerHTML = data.main.humidity + "%";
-        wind[0].innerHTML = Math.floor(data.wind.speed) + "km/h";
-        temperature[0].innerHTML= Math.floor(data.main.temp) + "ºc";
-        location[0].innerHTML = data.name;
 
-        if(data.weather[0].icon==="01d" || data.weather[0].icon==="01n") {
-            setWicon(clear_icon);
-        } else if (data.weather[0].icon==="02d" || data.weather[0].icon==="02n") {
-            setWicon(cloud_icon);
-        } else if (data.weather[0].icon==="03d" || data.weather[0].icon==="03n") {
-            setWicon(drizzle_icon);
-        } else if (data.weather[0].icon==="04d" || data.weather[0].icon==="04n") {
-            setWicon(drizzle_icon);
-        } else if (data.weather[0].icon==="09d" || data.weather[0].icon==="09n") {
-            setWicon(rain_icon);
-        } else if (data.weather[0].icon==="10d" || data.weather[0].icon==="10n") {
-            setWicon(rain_icon);
-        } else if (data.weather[0].icon==="13d" || data.weather[0].icon==="13n") {
-            setWicon(snow_icon);
-        } else {
-            setWicon(clear_icon);
+        if(data.cod === "404") {
+            return alert("City not found");
+        }
+
+        else{
+            const humidity = document.getElementsByClassName("humidity-percent");
+            const wind = document.getElementsByClassName("wind-rate");
+            const temperature = document.getElementsByClassName("weather-temp");
+            const location = document.getElementsByClassName("weather-location");
+            
+            humidity[0].innerHTML = data.main.humidity + "%";
+            wind[0].innerHTML = Math.floor(data.wind.speed) + "km/h";
+            temperature[0].innerHTML= Math.floor(data.main.temp) + "ºc";
+            location[0].innerHTML = data.name;
+
+            if(data.weather[0].icon==="01d" || data.weather[0].icon==="01n") {
+                setWicon(clear_icon);
+            } else if (data.weather[0].icon==="02d" || data.weather[0].icon==="02n") {
+                setWicon(cloud_icon);
+            } else if (data.weather[0].icon==="03d" || data.weather[0].icon==="03n") {
+                setWicon(drizzle_icon);
+            } else if (data.weather[0].icon==="04d" || data.weather[0].icon==="04n") {
+                setWicon(drizzle_icon);
+            } else if (data.weather[0].icon==="09d" || data.weather[0].icon==="09n") {
+                setWicon(rain_icon);
+            } else if (data.weather[0].icon==="10d" || data.weather[0].icon==="10n") {
+                setWicon(rain_icon);
+            } else if (data.weather[0].icon==="13d" || data.weather[0].icon==="13n") {
+                setWicon(snow_icon);
+            } else {
+                setWicon(clear_icon);
+            }
         }
     }
         
